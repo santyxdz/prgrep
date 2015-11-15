63f54b982898b99941b51a032502aefb0488e6fe
Santiago Montoya Angarita <santyxdz@gmail.com>
2015-11-15 15:53:54 -0500

    Objective 2a, "No hay nada para buscar"

--- prgrep.asm
+++ prgrep.asm
@@ -25,36 +25,87 @@
     mov rdi,0
     syscall
 %endmacro
+%macro return 1
+    mov rax,60
+    mov rdi,%1
+    syscall
+%endmacro
+%macro println 0
+    print nl,nlen
+%endmacro
 
 section .text
-;Procedimientos : Como un funcion
-procedimiento_ex:
-    print notFound,notFound
-ret
+;Procedimientos : Como una funcion
+; parameters
+;    for int and &points rdi,rsi,rdx,rcx,r8,r9
+;    for float and doubles xmm[0-7]
+;    for aditional on the stack and removed by the caller
+;    when called function gets control ret_add is [rsp] and
+;    1st memory paramaters is at [rsp+8]
+;call-save reg rbp, rbx, r12, r13, r14, r15
+; return
+;    int in rax or rdx;rax
+;    float xmm0 or xmm1:xmm0
 
-;MAIN
-    global _start ;Para que el enlazador sepa donde empezar
+;rax:int strlen(rdi=char*)
+strlen:
+    mov rdx, rdi
+    xor rax,rax ; count = 0
+    jmp overit
+    looptop:
+    inc rdx
+    inc rax
+    overit:
+    cmp byte[rdx],0
+    jnz looptop
+ret
+; end strlen
+
+;======================MAIN============================
+global _start ;Para que el enlazador sepa donde empezar
 _start: ;like main()
-    
-    ;call procedimiento_ex
-    ;print notFound,notFoundLen-5
-    pop    rsi              ; number of arguments (argc)
-    mov argc,rsi;
-    pop    rsi              ; the command itself (or programname as you like)
-    mov pname,rsi
-    pop    rsi              ; the pointer to the string
-    mov argv,rsi
-    exit
+        pop    rsi              ; number of arguments (argc)
+        cmp rsi,1 ;No arguments
+        je not_args_found
+        jg args_found
+        jl end
+    args_found:
+        dec rsi ; deleting name of program from arguments
+        mov [argc],rsi; ;saving argc argument
+        pop    rsi              ; the command itself (or programname as you like)
+        mov [pname],rsi ;saving name of program
+        pop    rsi              ; the pointer to the string
+        mov [argv],rsi          ;saving pointer
+        mov rax,[zero]
+        cmp [argc],rax ; argc==0?
+        ;rax=strlen(argv)
+        ;mov rdi,[argv]
+        ;call strlen
+        ; print(argv,strlen(argv))
+        ;mov rcx,rax
+        ;print [argv],rcx
+        jmp end
+    not_args_found:
+        print not_found,not_found_len ;print string
+        exit
+    end:
+        println
+        exit
 
 section .data ;variables inicializadas
     ; <name_var> <size> <init_val>
-    notFound db "No hay nada para buscar",0xA;10,13 | o12,o15 means \n\r
-    notFoundLen equ $ - notFound ;sizeof(notFound) equ is a directive
-    argc dd 0; dword
-    pname db "./prgrep",0
-    argv dd 0;
+    not_found db "No hay nada para buscar",0xA;10,13 | o12,o15 means \n\r
+    not_found_len equ $ - not_found ;sizeof(not_found) equ is a directive
+    nl db "",0xA ; \n
+    nlen equ $ - nl
+    zero dd 0 ; Int->4Bytes->Double Word
+    ;argc dd 0; dword
+    ;pname db "./prgrep",0
+    ;argv dd 0;
 section .bss ;varibles no inicializadas
     ; <name_var> <size> <how_many>
-    argc resq
     idarchivo resd 1 ; Archivo
     numlines resb 1; For lines
+    argc resd 1;
+    pname resb 10;
+    argv resd 1;
